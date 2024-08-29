b8037055ad1368fe797653e7582ad11bab47dab8
appdevs-lee <js100810081@gmail.com>
2024-08-29 09:34:00 +0900

    Change Image Asset

--- RPA-P/Profile/BusViewController.swift
+++ RPA-P/Profile/BusViewController.swift
@@ -10,8 +10,28 @@ import UIKit
 struct Bus {
     let name: String
     let content: String
-    let busType: String
+    let busType: KindsOfBusType
     let imageName: String
+}
+
+enum KindsOfBusType: String {
+    case small = "14인승"
+    case middle = "25~29인승"
+    case big = "41~45인승"
+    case superBig = "45~49인승"
+    
+    var imageName: String {
+        switch self {
+        case .small:
+            return "small"
+        case .middle:
+            return "middle"
+        case .big:
+            return "big"
+        case .superBig:
+            return "superBig"
+        }
+    }
 }
 
 private enum Index {
@@ -114,13 +134,13 @@ final class BusViewController: UIViewController {
     var selectedIndex: Int = 0
     
     let busList: [Bus] = [
-        Bus(name: "현대/쏠라티", content: "현대 쏠라티는 넓은 실내 공간과 편안한 좌석으로 장거리 여행에서도 쾌적한 승차감을 제공하며, 에어컨과 USB 입력 등의 현대적인 편의 시설을 갖춰 승객들이 편리하게 이용할 수 있습니다현대 쏠라티는 넓은 실내 공간과 편안한 좌석으로 장거리 여행에서도 쾌적한 승차감을 제공하며, 에어컨과 USB 입력 등의 현대적인 편의 시설을 갖춰 승객들이 편리하게 이용할 수 있습니다", busType: "14인승", imageName: "Solati"),
-        Bus(name: "현대/뉴카운티", content: "현대 뉴 카운티는 편안한 좌석과 최적화된 서스펜션으로 쾌적한 승차감을 제공하며, 에어컨과 맞춤형 수납공간 등 다양한 편의 시설로 고객에게 만족스러운 서비스를 선사합니다.", busType: "25~29인승", imageName: "NewCounty"),
-        Bus(name: "현대/유니버스", content: "현대 유니버스는 넓고 고급스러운 실내 공간과 공기 서스펜션을 통해 승객에게 편안한 승차감을 제공하며, 통합 에어컨과 조명 시스템으로 쾌적한 환경을 조성해 장거리 여행에 적합합니다​", busType: "41~45인승", imageName: "Universe"),
-        Bus(name: "기아/뉴그랜버드", content: "기아 뉴 그랜버드는 넓은 실내 공간과 고급스러운 인테리어로 승객들에게 편안함을 제공하며, 첨단 기술을 기반으로 한 탁월한 주행 성능과 부드러운 승차감을 자랑합니다. 또한, 현대적인 편의 시설을 갖추고 있어 장거리 여행에도 쾌적한 환경을 유지할 수 있습니다​", busType: "45~49인승", imageName: "NewGranbird"),
-        Bus(name: "현대/유니버스-프라임", content: "현대 유니버스-프라임은 넓은 좌석과 인체공학적 설계로 최상의 승차감을 제공하며, 에어컨과 멀티미디어 시스템 등 다양한 편의 시설로 고객에게 탁월한 서비스 경험을 선사합니다.", busType: "41~45인승", imageName: "UniversePrime"),
-        Bus(name: "현대/유니버스-노블", content: "현대 유니버스 노블은 고급스러운 실내 공간과 현대적인 편의 시설을 통해 승객들에게 뛰어난 서비스 경험을 제공합니다. 이 버스는 에어 서스펜션을 사용하여 부드러운 승차감을 제공하며, 조용한 실내 환경과 통합 공조 시스템으로 쾌적한 여행을 보장합니다", busType: "41~45인승", imageName: "UniverseNoble"),
-        Bus(name: "기아/뉴그랜버드-실크로드", content: "기아 뉴 그랜버드 실크로드는 넓고 고급스러운 실내 공간을 제공하며, 승객들에게 편안하고 쾌적한 여행 경험을 선사합니다. 이 버스는 강력한 엔진과 에어 서스펜션을 통해 부드러운 승차감을 제공하며, 최신 편의 시설을 갖추고 있어 장거리 이동에도 최적화되어 있습니다. 또한, 현대적인 디자인과 고급스러운 마감재로 스타일을 중시하는 고객에게 적합합니다​", busType: "45~49인승", imageName: "NewGranbirdSilkRoad"),
+        Bus(name: "현대/쏠라티", content: "현대 쏠라티는 넓은 실내 공간과 편안한 좌석으로 장거리 여행에서도 쾌적한 승차감을 제공하며, 에어컨과 USB 입력 등의 현대적인 편의 시설을 갖춰 승객들이 편리하게 이용할 수 있습니다현대 쏠라티는 넓은 실내 공간과 편안한 좌석으로 장거리 여행에서도 쾌적한 승차감을 제공하며, 에어컨과 USB 입력 등의 현대적인 편의 시설을 갖춰 승객들이 편리하게 이용할 수 있습니다", busType: .small, imageName: "Solati"),
+        Bus(name: "현대/뉴카운티", content: "현대 뉴 카운티는 편안한 좌석과 최적화된 서스펜션으로 쾌적한 승차감을 제공하며, 에어컨과 맞춤형 수납공간 등 다양한 편의 시설로 고객에게 만족스러운 서비스를 선사합니다.", busType: .middle, imageName: "NewCounty"),
+        Bus(name: "현대/유니버스", content: "현대 유니버스는 넓고 고급스러운 실내 공간과 공기 서스펜션을 통해 승객에게 편안한 승차감을 제공하며, 통합 에어컨과 조명 시스템으로 쾌적한 환경을 조성해 장거리 여행에 적합합니다​", busType: .big, imageName: "Universe"),
+        Bus(name: "기아/뉴그랜버드", content: "기아 뉴 그랜버드는 넓은 실내 공간과 고급스러운 인테리어로 승객들에게 편안함을 제공하며, 첨단 기술을 기반으로 한 탁월한 주행 성능과 부드러운 승차감을 자랑합니다. 또한, 현대적인 편의 시설을 갖추고 있어 장거리 여행에도 쾌적한 환경을 유지할 수 있습니다​", busType: .superBig, imageName: "NewGranbird"),
+        Bus(name: "현대/유니버스-프라임", content: "현대 유니버스-프라임은 넓은 좌석과 인체공학적 설계로 최상의 승차감을 제공하며, 에어컨과 멀티미디어 시스템 등 다양한 편의 시설로 고객에게 탁월한 서비스 경험을 선사합니다.", busType: .big, imageName: "UniversePrime"),
+        Bus(name: "현대/유니버스-노블", content: "현대 유니버스 노블은 고급스러운 실내 공간과 현대적인 편의 시설을 통해 승객들에게 뛰어난 서비스 경험을 제공합니다. 이 버스는 에어 서스펜션을 사용하여 부드러운 승차감을 제공하며, 조용한 실내 환경과 통합 공조 시스템으로 쾌적한 여행을 보장합니다", busType: .big, imageName: "UniverseNoble"),
+        Bus(name: "기아/뉴그랜버드-실크로드", content: "기아 뉴 그랜버드 실크로드는 넓고 고급스러운 실내 공간을 제공하며, 승객들에게 편안하고 쾌적한 여행 경험을 선사합니다. 이 버스는 강력한 엔진과 에어 서스펜션을 통해 부드러운 승차감을 제공하며, 최신 편의 시설을 갖추고 있어 장거리 이동에도 최적화되어 있습니다. 또한, 현대적인 디자인과 고급스러운 마감재로 스타일을 중시하는 고객에게 적합합니다​", busType: .superBig, imageName: "NewGranbirdSilkRoad"),
     ]
     
     override func viewDidLoad() {
@@ -158,7 +178,7 @@ extension BusViewController: EssentialViewMethods {
     }
     
     func initializeObjects() {
-        self.kindsOfBusImageView.image = .useCustomImage(self.busList[0].busType)
+        self.kindsOfBusImageView.image = .useCustomImage(self.busList[0].busType.imageName)
         self.busNameLabel.text = self.busList[0].name
         self.contentLabel.text = self.busList[0].content
         
@@ -310,7 +330,7 @@ extension BusViewController: UICollectionViewDelegateFlowLayout, UICollectionVie
         if collectionView == self.collectionView {
             self.selectedIndex = indexPath.row
             self.busCollectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
-            self.kindsOfBusImageView.image = .useCustomImage(self.busList[indexPath.row].busType)
+            self.kindsOfBusImageView.image = .useCustomImage(self.busList[indexPath.row].busType.imageName)
             self.busNameLabel.text = self.busList[indexPath.row].name
             self.contentLabel.text = self.busList[indexPath.row].content
             
@@ -331,7 +351,7 @@ extension BusViewController: UICollectionViewDelegateFlowLayout, UICollectionVie
             targetContentOffset.pointee = CGPoint(x: index * cellWidth - scrollView.contentInset.left, y: scrollView.contentInset.top)
             
             print("index: \(index)")
-            self.kindsOfBusImageView.image = .useCustomImage(self.busList[Int(index)].busType)
+            self.kindsOfBusImageView.image = .useCustomImage(self.busList[Int(index)].busType.imageName)
             self.busNameLabel.text = self.busList[Int(index)].name
             self.contentLabel.text = self.busList[Int(index)].content
             
